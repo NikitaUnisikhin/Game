089ae429b30ca4acc068051b3c93ce4226dbfdeb
Alexander Skomorokhov <skomor.ae@gmail.com>
2022-04-26 19:17:51 +0500

    Add mechanic to Doctor. Add him LivesBar. Fix his GroundCheck.

--- Assets/Scripts/Doctor.cs
+++ Assets/Scripts/Doctor.cs
@@ -1,12 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class Robber : Monster
+public class Doctor : Monster
 {
     [SerializeField]
     private float speed = 2.0F;
+    [SerializeField]
+    private int lives = 3;
+    [SerializeField]
+    private float rate = 2F;
 
-    private Spear spear;
+    public int Lives
+    {
+        get { return lives; }
+        set
+        {
+            lives = value;
+        }
+    }
+
+    private Shell shell;
     private Vector3 direction;
 
     private bool isFacingLeft = true;
@@ -19,12 +34,13 @@ public class Robber : Monster
 
     protected void Awake()
     {
-        spear = Resources.Load<Spear>("Spear");
+        shell = Resources.Load<Shell>("Shell");
         sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     protected void Start()
     {
+        InvokeRepeating("Shoot", rate, rate);
         direction = -transform.right;
     }
     protected void Update()
@@ -49,13 +65,23 @@ public class Robber : Monster
         }
     }
 
+    private void Shoot()
+    {
+        Vector3 position = transform.position; position.y += 0.4F;
+        Shell newShell = Instantiate(shell, position, shell.transform.rotation);
+
+        newShell.Parent = gameObject;
+        newShell.Sprite.flipX = !isFacingLeft;
+        newShell.Direction = -newShell.transform.right * speed / 2;
+    }
+
     private void Move()
     {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.5F + transform.right * direction.x * 0.5F, 0.1F);
 
         RaycastHit2D groundInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, 1f, groundLayers);
 
-        if (colliders.Length > 0 && colliders.All(x => !x.GetComponent<Character>() && !x.GetComponent<Spear>()) || groundInfo.collider == false)
+        if (colliders.Length > 0 && colliders.All(x => !x.GetComponent<Character>() && !x.GetComponent<Spear>() && !x.GetComponent<Shell>()) || groundInfo.collider == false)
         {
             isFacingLeft = !isFacingLeft;
             speed = -speed;
@@ -63,5 +89,15 @@ public class Robber : Monster
             transform.localScale = new Vector2(-transform.localScale.x, 1f);
         }
         rb.velocity = new Vector2(-speed, rb.velocity.y);
+    }
+
+    public override void ReceiveDamage()
+    {
+        Lives--;
+
+        rb.velocity = Vector3.zero;
+        rb.AddForce(transform.up * 8.0F, ForceMode2D.Impulse);
+
+        if (lives <= 0) Die();
     }
 }
