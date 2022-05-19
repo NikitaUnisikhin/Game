55afe87b42042bf2e62c6c935412566aeaeebfb2
Alexander Skomorokhov <skomor.ae@gmail.com>
2022-05-19 22:41:21 +0500

    Make Panel with text more readable, and add some scripts that deleted some times ago

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,13 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public AudioSource audio;
-
+    new public AudioSource audio;
     void Start()
     {
-        audio = GetComponent<AudioSource>();   
+        audio = GetComponent<AudioSource>();
     }
 
     public void ClickButton(string nameOfScene)
